9da4895ee326c1c7bccaed7a6cb799b35a075241
tpiros <2042718+tpiros@users.noreply.github.com>
2021-05-06 13:42:22 +0200

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addWatchTarget('./_tmp/style.css');
+
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' });
+  eleventyConfig.addPassthroughCopy('assets');
+};
